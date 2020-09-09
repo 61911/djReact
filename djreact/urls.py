9e771fee9b34dd119482e8ab3e1a9091466809a9
61911 <psp.11.jb@gmail.com>
2020-09-09 19:04:05 +0530

    update url for backendapi

--- djreact/urls.py
+++ djreact/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    re_path(".*", TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/', include("articles.api.urls")),
     path('api-auth/', include('rest_framework.urls')),
