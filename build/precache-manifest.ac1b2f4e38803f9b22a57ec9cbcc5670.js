24a8a322426151d5e8c612cf10ece71ca5f47e52
61911 <psp.11.jb@gmail.com>
2020-09-09 18:37:01 +0530

    npm build and set template

---
+++ build/precache-manifest.ac1b2f4e38803f9b22a57ec9cbcc5670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e1ca033da019b1f5ff1dc2f9837a33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d0e5f477bc2519d1c64",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "eb596a052f521100071a",
+    "url": "/static/css/main.ccfdfe5f.chunk.css"
+  },
+  {
+    "revision": "3d0e5f477bc2519d1c64",
+    "url": "/static/js/2.93d0a590.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.93d0a590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb596a052f521100071a",
+    "url": "/static/js/main.5d4900a3.chunk.js"
+  },
+  {
+    "revision": "d96159fb79fad195664d",
+    "url": "/static/js/runtime-main.2dc790a7.js"
+  }
+]);
